5de906c8d68ea92901f0a0b3eb974fc87ea8926a
Boydem <noyemdahan@gmail.com>
2023-03-08 18:43:20 +0200

    search-regions added

--- src/app/cmps/search-regions.tsx
+++ src/app/cmps/search-regions.tsx
@@ -1,3 +1,47 @@
 export function SearchRegions() {
-    return <section className='search-regions'></section>
+    const regions = [
+        {
+            id: 'all',
+            label: "I'm Flexible",
+            imgUrl: 'https://res.cloudinary.com/dsperrtyj/image/upload/v1678292986/all_np0zob.webp',
+        },
+        {
+            id: 'middle-east',
+            label: 'Middle East',
+            imgUrl: 'https://res.cloudinary.com/dsperrtyj/image/upload/v1678292986/middle-east_hxxpha.webp',
+        },
+        {
+            id: 'italy',
+            label: 'Italy',
+            imgUrl: 'https://res.cloudinary.com/dsperrtyj/image/upload/v1678292986/italy_fdawj0.webp',
+        },
+        {
+            id: 'south-america',
+            label: 'South America',
+            imgUrl: 'https://res.cloudinary.com/dsperrtyj/image/upload/v1678292986/south-america_drnwwl.webp',
+        },
+        {
+            id: 'france',
+            label: 'France',
+            imgUrl: 'https://res.cloudinary.com/dsperrtyj/image/upload/v1678292986/france_cwg5fh.webp',
+        },
+        {
+            id: 'usa',
+            label: 'United States',
+            imgUrl: 'https://res.cloudinary.com/dsperrtyj/image/upload/v1678292986/usa_z7kjby.webp',
+        },
+    ]
+    return (
+        <section className='search-regions'>
+            <h4>Search by region</h4>
+            <ul className='regions-list clean-list'>
+                {regions.map(region => (
+                    <li key={region.id}>
+                        <img src={region.imgUrl} alt='' />
+                        <span>{region.label}</span>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
 }
